89e5af76ba2d72df42f828c85959ec4de7b654a7
Hrishikesh T <hrishikesh@MAR88-137.kek.jp>
2017-05-27 23:17:38 +0900

    Create tempArray.c

---
+++ tempArray.c
@@ -0,0 +1,78 @@
+float tempin[8][7];
+for (int channel = 0; channel < 8; channel++) {
+  switch(channel) {
+    case 0:
+      tempin[0][0] = 22.4209;
+      tempin[0][1] = 22.9657;
+      tempin[0][2] = 23.5116;
+      tempin[0][3] = 24.8536;
+      tempin[0][4] = 24.6069;
+      tempin[0][5] = 25.7066;
+      tempin[0][6] = 26.2582;
+      break;
+    case 1:
+      tempin[1][0] = 22.6932;
+      tempin[1][1] = 23.2385;
+      tempin[1][2] = 23.5116;
+      tempin[1][3] = 24.0587;
+      tempin[1][4] = 25.1562;
+      tempin[1][5] = 25.9822;
+      tempin[1][6] = 26.5344;
+      break;
+    case 2:
+      tempin[2][0] = 22.6932;
+      tempin[2][1] = 22.9657;
+      tempin[2][2] = 23.5116;
+      tempin[2][3] = 24.0587;
+      tempin[2][4] = 24.8814;
+      tempin[2][5] = 25.7066;
+      tempin[2][6] = 26.2582;
+      break;
+    case 3:
+      tempin[3][0] = 21.8772;
+      tempin[3][1] = 22.1489;
+      tempin[3][2] = 22.9657;
+      tempin[3][3] = 23.2385;
+      tempin[3][4] = 24.0587;
+      tempin[3][5] = 25.1562;
+      tempin[3][6] = 26.4312;
+      break;
+    case 4:
+      tempin[4][0] = 22.6932;
+      tempin[4][1] = 22.6932;
+      tempin[4][2] = 23.5116;
+      tempin[4][3] = 23.7850;
+      tempin[4][4] = 25.1562;
+      tempin[4][5] = 25.7066;
+      tempin[4][6] = 26.2582;
+      break;
+    case 5:
+      tempin[5][0] = 22.6932;
+      tempin[5][1] = 22.9657;
+      tempin[5][2] = 23.7850;
+      tempin[5][3] = 24.3327;
+      tempin[5][4] = 25.1562;
+      tempin[5][5] = 26.2582;
+      tempin[5][6] = 26.5344;
+      break;
+    case 6:
+      tempin[6][0] = 22.6932;
+      tempin[6][1] = 22.9657;
+      tempin[6][2] = 23.5116;
+      tempin[6][3] = 24.0587;
+      tempin[6][4] = 24.8814;
+      tempin[6][5] = 25.7066;
+      tempin[6][6] = 26.2582;
+      break;
+    case 7:
+      tempin[7][0] = 21.8772;
+      tempin[7][1] = 22.1489;
+      tempin[7][2] = 22.9657;
+      tempin[7][3] = 23.2385;
+      tempin[7][4] = 24.0587;
+      tempin[7][5] = 25.4312;
+      tempin[7][6] = 26.1562;
+      break;
+    default: cout<<"Oops!"<<endl;
+  }
+}
